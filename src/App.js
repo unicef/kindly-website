71d865cea61f868a97cf1a51aaa0ba4294adf1d2
Victor Grau Serrat <lacabra@users.noreply.github.com>
2021-10-16 10:23:44 -0600

    BLD: addded SVGs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {HashLink as Link} from "react-router-hash-link";
+import { HashLink } from "react-router-hash-link";
+import { Link } from "react-router-dom";
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -7,6 +8,13 @@ import IconArrowDownWhite from './img/icon-arrow-down-white.svg';
 import IconArrowDownBlue from './img/icon-arrow-down-blue.svg';
 import IconSend from './img/icon-send.svg';
 import UNICEFLogo from './img/unicef-logo.png';
+import checkWithKindly from './img/check-with-kindly.svg';
+import checkMessage from './img/check-message.svg';
+import sendFinalMsg from './img/send-final-message.svg';
+import viewFeedback from './img/view-feedback.svg';
+import privacy from './img/kindly-privacy.svg';
+import openSource from './img/kindly-open-source.svg';
+import empower from './img/kindly-empowerment.svg';
 
 function Message(props){
   return(
@@ -36,10 +44,10 @@ function App() {
         </div>
 
         <footer className="cover-footer mt-auto">
-          <Link to="#about" className="text-decoration-none text-white">
+          <HashLink to="#about" className="text-decoration-none text-white">
             <span>Learn about Kindly</span><br/>
             <img src={IconArrowDownWhite} className="icon-arrow-down" alt="Downward Arrow"/>
-          </Link>
+          </HashLink>
         </footer>
 
         <div className="background container-fluid vh-100">
@@ -92,100 +100,132 @@ function App() {
         </div>
       </div>
 
-      <div className="px-4 p-5 cover-unicef" id="about">
-        <div className="row align-items-center g-5 py-5">
-          <div className="col-lg-5 offset-1">
-            <img src={UNICEFLogo} width="490" alt="UNICEF Logo" className="d-block mx-lg-auto img-fluid" loading="lazy"/>
-          </div>
-          <div className="col-lg-3 offset-2">
-            <p className="lead text-left">Kindly is the product of a collaboration between UNICEF and Gitanjali Rao, a young innovator.</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="px-4 p-5">
-        <div className="row align-items-center g-5 py-5">
-          <div className="col-lg-5 offset-1">
-            <h1 className="">Kindly is an API that uses Machine Learning to identify 'toxic' messages.</h1>  
-            <p className="lead">Kindly evaluates messages before they are sent and offers users the chance to modify them.</p>
-            <p className="lead">Kindly aims to make children feel safer by leveraging the latest advances in technology and by empowering children to solve this pressing challenge.</p>
-          </div>
-        </div>
-        <footer className="cover-footer mt-auto mx-auto text-center">
-          <Link to="#test-kindly" className="text-decoration-none text-blue">
-            <span>Test Kindly</span><br/>
-            <img src={IconArrowDownBlue} className="icon-arrow-down" alt="Downward Arrow"/>
-          </Link>
-        </footer>
-      </div>
-
-      <div className="px-4 p-5 background-blue text-white" id="test-kindly">
-        <div className="row align-items-center g-5 py-5 mb-5">
-          <div className="col-lg-5 offset-1">
-            <h1 className="mb-5">Kindly aims to end cyberbullying, one message at a time.</h1>  
-            <p className="lead"><b>Test Kindly for yourself!</b></p>
-            <p >Reply to the message and check your entry with Kindly to see if your message is toxic or considered cyberbullying.</p>
-          </div>
-          <div className="col-lg-5 offset-1">
-            <KindlyForm />
-          </div>
-        </div>
-        <footer className="cover-footer mt-5 mx-auto text-center">
-          <span>How Kindly works</span><br/>
-          <img src={IconArrowDownWhite} className="icon-arrow-down" alt="Downward Arrow"/>
-        </footer>
-      </div>
-
-      <div className="px-4 p-5">
-        <div className="row align-items-center g-5 py-5">
-          <div className="col-lg-7 text-center mx-auto">
-            <h1 className="">Think <i>Grammerly</i>,<br/> but for cyberbullying</h1>  
-            <p className="text-grey">When integrated with existing text-based platforms, Kindly allows users choose to check and edit their messages for toxic intent.</p>
-          </div>
-        </div>
-        <div className="row align-items-center g-5 py-5 px-5">
-          <div className="col-lg-4 text-center mx-auto">
-            <p><b>Check the message</b></p>
-            <p>Users prompt Kindly to evaluate their message draft for toxic text or cyberbullying intent</p>
-          </div>
-          <div className="col-lg-4 text-center mx-auto">
-            <p><b>View feedback</b></p>
-            <p>Users prompt Kindly to evaluate their message draft for toxic text or cyberbullying intent</p>
-          </div>
-          <div className="col-lg-4 text-center mx-auto">
-            <p><b>Send final message</b></p>
-            <p>Users prompt Kindly to evaluate their message draft for toxic text or cyberbullying intent</p>
-          </div>
-        </div>
-        <footer className="cover-footer mt-auto mx-auto text-center">
-          <Link to="#why-kindly" className="text-decoration-none text-blue">
-            <span>Why Kindly?</span><br/>
-            <img src={IconArrowDownBlue} className="icon-arrow-down" alt="Downward Arrow"/>
-          </Link>
-        </footer>
-      </div>
-
-      <div className="px-4 p-5 background-black">
-        <div className="row align-items-center g-5 py-5">
-          <div className="col-lg-7 text-center mx-auto text-white text-center">
-            <h2>One-third of the globe’s youth is bullied. It affects everyone: 
-            those who are bullied, those who bully others, and those who witness
-            bullying. And it doesn’t stop in the physical world. It happens
-            online, too.</h2>
+      <div className="p-5 cover-unicef" id="about">
+        <div className="container-lg">
+          <div className="row align-items-center g-5 py-5">
+            <div className="col-lg-5 offset-1">
+              <img src={UNICEFLogo} width="490" alt="UNICEF Logo" className="d-block mx-lg-auto img-fluid" loading="lazy"/>
+            </div>
+            <div className="col-lg-3 offset-2">
+              <p className="lead text-left">Kindly is the product of a collaboration between UNICEF and Gitanjali Rao, a young innovator.</p>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="px-4 p-5">
+      <div className="p-5">
+        <div className="container-lg p-5">
+          <div className="row align-items-center g-5 py-5">
+            <div className="col-lg-5 offset-1">
+              <h1 className="">Kindly is an API that uses Machine Learning to identify 'toxic' messages.</h1>  
+              <p className="lead">Kindly evaluates messages before they are sent and offers users the chance to modify them.</p>
+              <p className="lead">Kindly aims to make children feel safer by leveraging the latest advances in technology and by empowering children to solve this pressing challenge.</p>
+            </div>
+            <div className="col-lg-5">
+              <img 
+                src={checkWithKindly}
+                alt="Check With Kindly"
+                className="w-100"/> 
+            </div>
+          </div>
+          <footer className="cover-footer mt-auto mx-auto text-center">
+            <HashLink to="#test-kindly" className="text-decoration-none text-blue">
+              <span>Test Kindly</span><br/>
+              <img src={IconArrowDownBlue} className="icon-arrow-down" alt="Downward Arrow"/>
+            </HashLink>
+          </footer>
+        </div>
+      </div>
+
+      <div className="p-5 background-blue text-white" id="test-kindly">
+        <div className="container-lg">
+          <div className="row align-items-center g-5 py-5 mb-5">
+            <div className="col-lg-5 offset-1">
+              <h1 className="mb-5">Kindly aims to end cyberbullying, one message at a time.</h1>  
+              <p className="lead"><b>Test Kindly for yourself!</b></p>
+              <p >Reply to the message and check your entry with Kindly to see if your message is toxic or considered cyberbullying.</p>
+            </div>
+            <div className="col-lg-5 offset-1">
+              <KindlyForm />
+            </div>
+          </div>
+          <footer className="cover-footer mt-5 mx-auto text-center">
+            <HashLink to="#how-kindly-works" className="text-decoration-none text-white">
+              <span>How Kindly works</span><br/>
+              <img src={IconArrowDownWhite} className="icon-arrow-down" alt="Downward Arrow"/>
+            </HashLink>
+          </footer>
+        </div>
+      </div>
+
+      <div className="p-5" id="how-kindly-works">
+        <div className="container-lg">
+          <div className="row align-items-center pt-5 pb-3">
+            <div className="col-lg-7 text-center mx-auto">
+              <h1 className="">Think <i>Grammerly</i>,<br/> but for cyberbullying</h1>  
+              <p className="text-grey">When integrated with existing text-based platforms, Kindly allows users choose to check and edit their messages for toxic intent.</p>
+            </div>
+          </div>
+          <div className="row px-5 pb-5 align-items-start">
+            <div className="col-lg-3 text-center mx-auto px-5">
+              <img 
+                src={checkMessage}
+                alt="Check the message"
+                className="pl-3"/>
+              <p><b>Check the message</b></p>
+              <p>Users prompt Kindly to evaluate their message draft
+              for toxic text or cyberbullying intent</p>
+            </div>
+            <div className="col-lg-3 text-center mx-auto">
+              <img
+                src={viewFeedback}
+                alt="View Feedback"/>
+              <p><b>View feedback</b></p>
+              <p>Feedback on the message offers users the chance
+              to edit their original message</p>
+            </div>
+            <div className="col-lg-3 text-center mx-auto px-5">
+              <img
+                src={sendFinalMsg}
+                alt="Send Final Message"
+                className="pr-3"/>
+              <p><b>Send final message</b></p>
+              <p>Users choose to send the final message after seeing
+              and/or responding to the feedback</p>
+            </div>
+          </div>
+          <footer className="cover-footer mt-auto mx-auto text-center">
+            <HashLink to="#why-kindly" className="text-decoration-none text-blue">
+              <span>Why Kindly?</span><br/>
+              <img src={IconArrowDownBlue} className="icon-arrow-down" alt="Downward Arrow"/>
+            </HashLink>
+          </footer>
+        </div>
+      </div>
+
+      <div className="p-5 background-black">
+        <div className="container-lg">
+          <div className="row align-items-center g-5 py-5">
+            <div className="col-lg-7 text-center mx-auto text-white text-center">
+              <h2>One-third of the globe’s youth is bullied. It affects everyone: 
+              those who are bullied, those who bully others, and those who witness
+              bullying. And it doesn’t stop in the physical world. It happens
+              online, too.</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-lg p-5" id="why-kindly">
         <div className="row align-items-center g-5 py-5">
           <div className="col-lg-7 text-center mx-auto">
             <h1 className="">Kindly is a uniquely qualified solution to eliminate cyberbullying</h1>  
             <p className="text-grey">Partners Gitanjali Rao and UNICEF provide the principles that make Kindly an excellent digital tool that empowers children.</p>
           </div>
         </div>
-        <div className="row align-items-center g-5 py-5 px-5">
+        <div className="row align-items-center px-5">
           <div className="col-lg-4 text-center mx-auto">
-            <p>&nbsp;</p>
+            <img src={privacy} alt="Children's data privacy" />
           </div>
           <div className="col-lg-5 mx-auto">
             <p className="unicef-blue-section-header">Protection of childen's rights and data</p>
@@ -198,7 +238,7 @@ function App() {
           </div>
         </div>
 
-        <div className="row align-items-center g-5 py-5 px-5">
+        <div className="row align-items-center px-5">
           <div className="col-lg-5 mx-auto">
             <p className="unicef-blue-section-header">An open-source solution for anyone to contribute</p>
             <p>By developing Kindly as an open-source solution, UNICEF encourages
@@ -208,13 +248,13 @@ function App() {
             different communities.</p>
           </div>
           <div className="col-lg-4 text-center mx-auto">
-            <p>&nbsp;</p>
+            <img src={openSource} alt="Open-source solution" />
           </div>
         </div>
 
-        <div className="row align-items-center g-5 py-5 px-5">
+        <div className="row align-items-center px-5">
           <div className="col-lg-4 text-center mx-auto">
-            <p>&nbsp;</p>
+            <img src={empower} alt="Empower anyone to contribute" />
           </div>
           <div className="col-lg-5 mx-auto">
             <p className="unicef-blue-section-header">Engaging children directly in a solution to cyberbullying</p>
@@ -226,7 +266,7 @@ function App() {
         </div>
       </div>
 
-      <div className="px-4 p-5">
+      <div className="container-lg p-5">
         <div className="row align-items-center g-5 py-5">
           <div className="col-lg-7 mx-auto">
           <h2>Frequently Asked Questions</h2>
@@ -235,49 +275,75 @@ function App() {
         </div>
       </div>
 
-      <div className="px-4 p-5 background-blue text-white">
-        <div className="row align-items-center g-5 py-5">
-          <div className="col-lg-7 mx-auto text-center">
-            <h2>Join us in ending cyberbullying</h2>
+      <div className="p-5 background-blue text-white">
+        <div className="container-lg">
+          <div className="row align-items-center g-5 py-5">
+            <div className="col-lg-7 mx-auto text-center">
+              <h2>Join us in ending cyberbullying</h2>
+            </div>
           </div>
-        </div>
-        <div className="row align-items-center g-5 py-5">
-          <div className="col-lg-4 mx-auto text-center">
-            <p className="lead">Stay updated</p>
-            <p>Keep up with Kindly’s progress by accessing the UNICEF/Kindly software repository</p>
-            <Button variant="outline-light" style={{minWidth: "12em"}}>Software Repository</Button>
-          </div>
-          <div className="col-lg-4 mx-auto text-center">
-            <p className="lead">Learn more</p>
-            <p>Connect with Kindly to learn more about Kindly or to explore a partnership role with us</p>
-            <Button variant="outline-light" style={{minWidth: "12em"}}>Email us</Button>
+          <div className="row align-items-center g-5 py-5">
+            <div className="col-lg-4 mx-auto text-center">
+              <p className="lead">Stay updated</p>
+              <p>Keep up with Kindly’s progress by accessing the UNICEF/Kindly software repository</p>
+              <Button 
+                variant="outline-light" 
+                style={{minWidth: "12em"}}
+                href="https://github.com/unicef/kindly"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Software Repository
+              </Button>
+            </div>
+            <div className="col-lg-4 mx-auto text-center">
+              <p className="lead">Learn more</p>
+              <p>Connect with Kindly to learn more about Kindly or to explore a partnership role with us</p>
+              <Button 
+                variant="outline-light"
+                style={{minWidth: "12em"}}
+                href="mailto:kindly@unicef.org"
+                rel="noreferrer"
+              >
+                Email us
+              </Button>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="px-4 p-5 background-black text-white">
-        <div className="row align-items-center g-5 py-5">
-          <div className="col-lg-4 mx-auto">
-            <h1>Kindly</h1>
-            <p>Kindly is he product of innovator Gitanjali Rao and UNICEF's collaboration</p>
-          </div>
-          <div className="col-lg-4 mx-auto text-white">
-            <div className="row mb-2">
-              <Link to="#test" className="text-decoration-none text-white ">
-                <span>Test Kindly</span>
-              </Link>
+      <div className="p-5 background-black text-white">
+        <div className="container-lg">
+          <div className="row align-items-center g-5 py-5">
+            <div className="col-lg-5 mx-auto">
+              <h1>Kindly</h1>
+              <p>Kindly is he product of innovator Gitanjali Rao and UNICEF's collaboration</p>
             </div>
-            <div className="row mb-2">
-              <Link to="https://github.org/unicef/kindly" className="text-decoration-none text-white">
-                <span>Access Software Repository</span>
-              </Link>
+            <div className="col-lg-3 mx-auto text-white">
+              <div className="row mb-2">
+                <HashLink to="#test-kindly" className="text-decoration-none text-white ">
+                  Test Kindly
+                </HashLink>
+              </div>
+              <div className="row mb-2">
+                <a
+                  href="https://github.com/unicef/kindly" 
+                  className="text-decoration-none text-white"
+                  target="_blank"
+                  rel="noreferrer">
+                  Access Software Repository
+                </a>
+              </div>
+              <div className="row mb-2">
+                <a 
+                  className="text-decoration-none text-white"
+                  href="mailto:kindly@unicef.org"
+                  rel="noreferrer"
+                >
+                  Contact Kindly
+                </a>
+              </div>
             </div>
-            <div className="row mb-2">
-              <Link to="#test" className="text-decoration-none text-white">
-                <span>Contact Kindly</span>
-              </Link>
-            </div>
-            
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.body
 );
