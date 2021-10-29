9d3e9f6c9e716c327235061c307f5e5c46f77f6e
Victor Grau Serrat <lacabra@users.noreply.github.com>
2021-10-29 16:17:05 -0600

    BLD: contribute page layout

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Main from './Main';
 import Form from './Form';
+import Contribute from './Contribute';
 
 const App = () => {
   return (
     <Switch> {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path='/' component={Main}></Route>
       <Route exact path='/form' component={Form}></Route>
+      <Route exact path='/contribute' component={Contribute}></Route>
     </Switch>
   );
 }
